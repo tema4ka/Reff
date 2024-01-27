73de5b2037fa795784c87e25d4357005582383ce
tema4ka <99036220+tema4ka@users.noreply.github.com>
2024-01-27 12:23:29 +0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,20 @@
+
+token = "6326729421:AAH2EcgWD_-s1jOAUPegNWG-82lfLNq85V8"
+
+botname = 'citymone_bot'
+
+admins = [1023396221] #админы бота через запятую
+
+LITTLE_SUBCOIN_TO_GET_SUBS = 30 #минимум сабкоинов для раскрутки канала
+
+ref_count = 2 #реферальный бонус (сабкоины)
+
+REF_BONUS = 2
+
+FINE_FOR_UNSUBSCRIBING = -5 #Число должно быть отрицательным и целым
+
+SUBSCRIPTION_TERM = 7 #Какое время юзер должен оьязательно быть подписан на канал?
+
+LINK_TO_INTRODUCTION_AND_RULES = 'https://telegra.ph/Vvedenie-v-bota-a-takzhe-pravila-06-09' #ссылка на правила и инструкцию
+
+LINK_TO_CHAT_OF_BOT = '@kuznyvtebe' #ссылка на чат
